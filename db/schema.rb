e04288505a1f8fd6de818f557155e73beb80e1c9
deadsuperhero <sean@deadsuperhero.com>
2018-01-08 15:37:04 -0800

    entries should belong to lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180108214239) do
+ActiveRecord::Schema.define(version: 20180108233545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entries", force: :cascade do |t|
+    t.string "name"
+    t.boolean "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
